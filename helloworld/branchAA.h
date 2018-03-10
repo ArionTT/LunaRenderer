9d0badff3dbfd6f46c957df265abefa4c4c881d0
ArionTT <260569953@qq.com>
2018-03-10 23:00:43 +0800

    这是AA测试仓库

---
+++ helloworld/branchAA.h
@@ -0,0 +1,16 @@
+
+
+#include<iostream>
+
+
+class AA
+{
+public:
+	float a;
+	int b;
+public:
+	AA(){}
+public:
+	float getAA(){return a;}
+}
+
--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+#include"branchAA.h"
 
 int main()
 {
+AA a;
+
 std::cout<<"Hello World"<<std::endl;
 system("PAUSE");
 return 0;
